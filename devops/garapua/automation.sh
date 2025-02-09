7c6138fb772380546a352c194f37ba599ffcb7e9
Caio Vinicius <caio20122011@gmail.com>
2025-02-09 00:03:29 -0300

    chore: add k8s

---
+++ devops/garapua/automation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+origem="/garapua/ARQUIVOS"
+destino="/garapua/ARQUIVOS_PROCESSADOS"
+
+# Move e renomeia os arquivos
+for arquivo in "$origem"/*; do
+    if [ -f "$arquivo" ]; then
+        timestamp=$(date +"%Y%m%d_%H%M%S")
+        nome_arquivo=$(basename "$arquivo")
+        novo_nome="${timestamp}_${nome_arquivo}"
+        mv "$arquivo" "$destino/$novo_nome"
+        echo "Movido: $arquivo -> $destino/$novo_nome"
+    fi
+done
